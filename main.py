f4a188c430c15fc140b41f6a52e5f304e87e1cdc
Cratior <55932656+Cratior@users.noreply.github.com>
2023-06-06 22:41:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,59 @@
-from flask import Flask, request, send_file
+import tkinter as tk
+from tkinter import filedialog
 from PIL import Image, ImageDraw
 import json
-import io
 
-app = Flask(__name__)
+# Open file explorer to select black and white image
+root = tk.Tk()
+root.withdraw()
+image_path = filedialog.askopenfilename(title="Select Black and White Image")
 
-@app.route('/generate_blueprint', methods=['POST'])
-def generate_blueprint():
-    image = Image.open(request.files['image']).convert("1")
-    scaling_factor = float(request.form['scale'])
+# Load the image and convert it to black and white
+image = Image.open(image_path).convert("1")
 
-    blueprint = {
-        "center": 10.0,
-        "parts": [],
-        "stages": [],
-        "rotation": 0.0,
-        "offset": {"x": 0.0, "y": 0.0},
-        "interiorView": True
-    }
+# Create a blank blueprint
+blueprint = {
+    "center": 10.0,
+    "parts": [],
+    "stages": [],
+    "rotation": 0.0,
+    "offset": {"x": 0.0, "y": 0.0},
+    "interiorView": True
+}
 
-    draw = ImageDraw.Draw(image)
-    width, height = image.size
-    pixels = image.load()
+# Define the scaling factor for the output
+scaling_factor = 0.5  # Adjust this value as desired
 
-    for y in range(height):
-        for x in range(width):
-            if pixels[x, y] == 0:  # black pixel
-                scaled_x = x * scaling_factor
-                scaled_y = (height - y - 1) * scaling_factor
-                fuel_tank = {
-                    "n": "Fuel Tank",
-                    "p": {"x": float(scaled_x), "y": float(scaled_y)},
-                    "o": {"x": 1.0, "y": 1.0, "z": 0.0},
-                    "t": "-Infinity",
-                    "N": {
-                        "width_original": 2.0,
-                        "width_a": 2.0,
-                        "width_b": 2.0,
-                        "height": 1.0,
-                        "fuel_percent": 1.0
-                    },
-                    "T": {"color_tex": "_", "shape_tex": "_"}
-                }
-                blueprint["parts"].append(fuel_tank)
+# Extract the black outlines from the image
+draw = ImageDraw.Draw(image)
+width, height = image.size
+pixels = image.load()
 
-    output_file = io.BytesIO()
-    with open(output_file, "w") as f:
-        json.dump(blueprint, f, indent=4)
+for y in range(height):
+    for x in range(width):
+        if pixels[x, y] == 0:  # black pixel
+            # Add fuel tank at the position of the black pixel with scaling
+            scaled_x = x * scaling_factor
+            scaled_y = (height - y - 1) * scaling_factor
+            fuel_tank = {
+                "n": "Fuel Tank",
+                "p": {"x": float(scaled_x), "y": float(scaled_y)},
+                "o": {"x": 1.0, "y": 1.0, "z": 0.0},
+                "t": "-Infinity",
+                "N": {
+                    "width_original": 2.0,
+                    "width_a": 2.0,
+                    "width_b": 2.0,
+                    "height": 1.0,
+                    "fuel_percent": 1.0
+                },
+                "T": {"color_tex": "_", "shape_tex": "_"}
+            }
+            blueprint["parts"].append(fuel_tank)
 
-    output_file.seek(0)
-    return send_file(output_file, as_attachment=True, attachment_filename="blueprint.txt")
+# Save the modified blueprint to a file
+output_file = "blueprint.txt"
+with open(output_file, "w") as f:
+    json.dump(blueprint, f, indent=4)
 
-if __name__ == '__main__':
-    app.run()
+print(f"Modified blueprint saved to {output_file}")
